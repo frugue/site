7ce5fdcf53e0cb7d2df66ef967725825d1a9d5d4
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-22 01:01:13 +0600

    https://github.com/frugue/store/issues/4

--- app/code/TemplateMonster/Megamenu/Observer/PrepareCategory.php
+++ app/code/TemplateMonster/Megamenu/Observer/PrepareCategory.php
@@ -1,33 +1,32 @@
 <?php
 namespace TemplateMonster\Megamenu\Observer;
-
+use Magento\Catalog\Model\Category as C;
 use Magento\Framework\Event\ObserverInterface;
-
-use TemplateMonster\Megamenu\Helper\Data;
-
-class PrepareCategory implements ObserverInterface
-{
-	protected $_helper;
-
-	public function __construct(
-		Data $helper
-	) {
-		$this->_helper = $helper;
-	}
-
-	public function execute(\Magento\Framework\Event\Observer $observer)
-	{
-		$category = $observer->getCategory();
+use Magento\Framework\Event\Observer as O;
+// 2020-01-22
+class PrepareCategory implements ObserverInterface {
+	/**
+	 * 2020-01-22
+	 * @param O $o
+	 */
+	function execute(O $o) {
+		$c = $o['category']; /** @var C $c */
 		/**
 		 * 2020-01-20 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
 		 * «Uninitialized string offset: 0 in app/code/TemplateMonster/Megamenu/Observer/PrepareCategory.php on line 25»
 		 * https://github.com/frugue/store/issues/4
 		 */
-		if ($image = $category->getMmImage()) {
-			if (isset($image['delete']) && $image['delete']) {
-				$category->setData('mm_image_additional_data', ['delete' => true]);
-			} else {
-				$category->setMmImage($image[0]['name']);
+		if ($i = $c['mm_image']) { /** @var array(int|string => mixed) $i */
+			if (dfa($i, 'delete')) {
+				$c['mm_image_additional_data'] = ['delete' => true];
+			} 
+			else {
+				/**
+				 * 2020-01-20 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+				 * «Undefined index: name in app/code/TemplateMonster/Megamenu/Observer/PrepareCategory.php on line 30»
+				 * when a category is saved on a store level: https://github.com/frugue/store/issues/4
+				 */
+				$c['mm_image'] = dfa_deep($i, '0/name');
 			}
 		}
 	}
